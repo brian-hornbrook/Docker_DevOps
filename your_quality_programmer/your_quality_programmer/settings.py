544aeb027039aee54e28cb5ca4acbea156c874f6
Brian Hornbrook <brian_hornbrook@outlook.com>
2025-12-26 20:58:29 -0600

    connected django to nginx container

--- your_quality_programmer/your_quality_programmer/settings.py
+++ your_quality_programmer/your_quality_programmer/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-6eql$hw&__(&x5$7ik0si1*6q&xm_9-@%*t0hdrm)@)x%rs6u!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["yourhonestprogrammer.com", "localhost"]
 
 
 # Application definition
