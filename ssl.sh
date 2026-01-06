49bde935e6a5a439e5567ffce0258ae7e265bac2
Brian Hornbrook <brian_hornbrook@outlook.com>
2026-01-06 16:28:15 -0600

    added certbot folder

--- ssl.sh
+++ ssl.sh
@@ -4,3 +4,11 @@ docker compose run --rm certbot certbot certonly --webroot \
   --webroot-path=/var/lib/letsencrypt \
   -d yourhonestprogrammer.com -d www.yourhonestprogrammer.com \
   --email yourqualityprogrammer@gmail.com --agree-tos --non-interactive
+
+docker run -it --rm \
+  -v /etc/letsencrypt:/etc/letsencrypt \
+  -v /var/lib/letsencrypt:/var/lib/letsencrypt \
+  -v /var/www/certbot:/var/www/html \
+  certbot/certbot certonly --webroot -w /var/www/html \
+  -d yourdomain.com -d www.yourdomain.com \
+  --email your_email@example.com --agree-tos
