50839639406e7252203f3ef29189f99d351e02f2
Brian Hornbrook <brian_hornbrook@outlook.com>
2026-01-06 16:59:15 -0600

    added certbot volumes and command

--- ssl.sh
+++ ssl.sh
@@ -6,9 +6,9 @@ docker compose run --rm certbot certbot certonly --webroot \
   --email yourqualityprogrammer@gmail.com --agree-tos --non-interactive
 
 docker run -it --rm \
-  -v /etc/letsencrypt:/etc/letsencrypt \
-  -v /var/lib/letsencrypt:/var/lib/letsencrypt \
-  -v /var/www/certbot:/var/www/html \
+  -v ./certbot/etc/letsencrypt:/etc/letsencrypt \
+  -v ./certbot/var/lib/letsencrypt:/var/lib/letsencrypt \
+  -v ./certbot/var/www/html:/var/www/html \
   certbot/certbot certonly --webroot -w /var/www/html \
-  -d yourdomain.com -d www.yourdomain.com \
-  --email your_email@example.com --agree-tos
+  -d yourhonestprogrammer.com -d www.yourhonestprogrammer.com \
+  --email yourqualityprogrammer@gmail.com --agree-tos
